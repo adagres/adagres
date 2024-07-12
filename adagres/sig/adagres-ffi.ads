75b3bf62b67f19dc98f0f2a4a1d15a82929493b0
Yurii Rashkovskii <yrashk@gmail.com>
2024-07-12 06:44:37 -0700

    Problem: presence of `Get_Current_Command_Id_Wrap`
    What is it for?
    
    Solution: remove it, it came from earlier experiments

--- adagres/sig/adagres-ffi.ads
+++ adagres/sig/adagres-ffi.ads
@@ -11,9 +11,6 @@ is
    function Get_Current_Command_Id (Used : Interfaces.C.Extensions.bool) return Command_Id with
      Import, Convention => C, External_Name => "GetCurrentCommandId";
 
-   function Get_Current_Command_Id_Wrap (Used : Interfaces.C.Extensions.bool) return Command_Id with
-     Inline_Always;
-
    function Memory_Context_Alloc
      (Context : Memory_Context; Size : Interfaces.C.size_t) return System.Address with
      Import, Convention => C, External_Name => "MemoryContextAlloc";
